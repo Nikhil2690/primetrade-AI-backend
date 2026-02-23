af423b36d63d80069dd01333fac53362280f55bd
Nikhil2690 <nikhiladwani3@gmail.com>
2026-02-23 11:35:07 +0530

    adding proper origin in app.js to fetch from .env

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser"
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: process.env.CORS_ORIGIN, 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
